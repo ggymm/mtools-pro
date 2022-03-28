decd39b2d977d67ef7ca5b2893f98b1ff8f331e1
gongym <1967988842@qq.com>
2022-03-28 17:46:02 +0800

    重构Linux命令搜索

--- src/main/java/com/ggymm/mtools/modules/linux/controller/LinuxCommandController.java
+++ src/main/java/com/ggymm/mtools/modules/linux/controller/LinuxCommandController.java
@@ -2,19 +2,15 @@ package com.ggymm.mtools.modules.linux.controller;
 
 import cn.hutool.core.util.StrUtil;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.TextFieldListCell;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -36,7 +32,9 @@ public class LinuxCommandController implements Initializable {
     public AnchorPane root;
     public TextField keyword;
     public Button search;
-    public ListView<CommandItem> commandList;
+    public TableView<CommandItem> commandList;
+    public TableColumn<CommandItem, String> commandName;
+    public TableColumn<CommandItem, String> commandDescription;
 
     public List<CommandItem> commandItemList = new ArrayList<>();
     public Map<String, Stage> commandWebviewMap = new ConcurrentHashMap<>();
@@ -59,14 +57,23 @@ public class LinuxCommandController implements Initializable {
             this.commandList.setMinHeight(newValue.doubleValue() - otherHeight);
         });
 
+        this.commandName.prefWidthProperty().bind(this.commandList.widthProperty().multiply(0.25));
+        this.commandName.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getName()));
+
+        this.commandDescription.prefWidthProperty().bind(this.commandList.widthProperty().multiply(0.7));
+        this.commandDescription.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getDescription()));
+
         new Thread(() -> {
             final Set<String> commands = LinuxCommand.commands.keySet();
             for (String command : commands) {
                 final Object commandInfo = LinuxCommand.commands.get(command);
                 final Map<?, ?> commandInfoMap = (Map<?, ?>) commandInfo;
-                this.commandItemList.add(new CommandItem(
-                        String.valueOf(commandInfoMap.get("n")),
-                        String.valueOf(commandInfoMap.get("d"))));
+                this.commandItemList.add(
+                        new CommandItem(
+                                String.valueOf(commandInfoMap.get("n")),
+                                String.valueOf(commandInfoMap.get("d"))
+                        )
+                );
             }
 
             Platform.runLater(() -> this.commandList.getItems().addAll(this.commandItemList));
@@ -97,42 +104,40 @@ public class LinuxCommandController implements Initializable {
             }).start();
         });
 
-        // 监听点击事件
-        this.commandList.setCellFactory(param -> {
-            TextFieldListCell<CommandItem> cell = new TextFieldListCell<>();
-            cell.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
-                if (event.getTarget() instanceof StackPane || cell.getItem() == null) {
-                    return;
-                }
-                String itemName = cell.getItem().name;
-                if (this.commandWebviewMap.containsKey(itemName)) {
-                    this.commandWebviewMap.get(itemName).toFront();
-                    return;
-                }
-
-                // 创建stage
-                final Stage stage = new Stage();
-
-                // 创建webview
-                final WebView webView = new WebView();
-                final Scene scene = new Scene(webView, 640, 640);
-                stage.setTitle("Linux命令");
-                stage.setScene(scene);
-
-                // 渲染页面
-                final WebEngine engine = webView.getEngine();
-                final URL markdown = getClass().getResource("/commands/linux/html/" + itemName + ".md.html");
-                if (markdown != null) {
-                    engine.load(markdown.toExternalForm());
-                }
-                engine.getLoadWorker().stateProperty().addListener((ObservableValue<? extends Worker.State> ov, Worker.State oldState, Worker.State newState) -> {
-                    if (newState == Worker.State.SUCCEEDED) {
-                        stage.show();
-                        this.commandWebviewMap.put(itemName, stage);
+        this.commandList.setRowFactory(tv -> {
+            TableRow<CommandItem> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    String itemName = row.getItem().name;
+                    if (this.commandWebviewMap.containsKey(itemName)) {
+                        this.commandWebviewMap.get(itemName).toFront();
+                        return;
                     }
-                });
+
+                    // 创建stage
+                    final Stage stage = new Stage();
+
+                    // 创建webview
+                    final WebView webView = new WebView();
+                    final Scene scene = new Scene(webView, 640, 640);
+                    stage.setTitle("Linux命令");
+                    stage.setScene(scene);
+
+                    // 渲染页面
+                    final WebEngine engine = webView.getEngine();
+                    final URL markdown = getClass().getResource("/commands/linux/html/" + itemName + ".md.html");
+                    if (markdown != null) {
+                        engine.load(markdown.toExternalForm());
+                    }
+                    engine.getLoadWorker().stateProperty().addListener((ObservableValue<? extends Worker.State> ov, Worker.State oldState, Worker.State newState) -> {
+                        if (newState == Worker.State.SUCCEEDED) {
+                            stage.show();
+                            this.commandWebviewMap.put(itemName, stage);
+                        }
+                    });
+                }
             });
-            return cell;
+            return row;
         });
     }
 
