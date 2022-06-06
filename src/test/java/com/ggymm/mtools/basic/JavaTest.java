0acc553a3a399ddcaca5f6fc398190839683fca3
gongym <1967988842@qq.com>
2022-06-06 08:51:14 +0800

    提交测试代码

---
+++ src/test/java/com/ggymm/mtools/basic/JavaTest.java
@@ -0,0 +1,12 @@
+package com.ggymm.mtools.basic;
+
+/**
+ * @author gongym
+ * @version 创建时间: 2022-05-12 16:36
+ */
+public class JavaTest {
+
+    public static void main(String[] args) {
+        System.out.println(0 | 1 | 0);
+    }
+}
---
+++ src/test/java/com/ggymm/mtools/utils/TestDPI.java
@@ -0,0 +1,11 @@
+package com.ggymm.mtools.utils;
+
+/**
+ * @author gongym
+ * @version 创建时间: 2022-05-31 16:19
+ */
+public class TestDPI {
+
+    public static void main(String[] args) {
+    }
+}
